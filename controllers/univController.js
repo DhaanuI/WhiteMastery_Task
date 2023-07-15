d080f53a97966f51a9b451d8a06084b9252c7517
DhaanuI <dhaanu97@gmail.com>
2023-07-15 15:22:48 +0530

    clean MVC done

---
+++ controllers/univController.js
@@ -0,0 +1,106 @@
+const moment = require("moment");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { UnivModel } = require("../model/UnivModel");
+
+
+const univRegister = async (req, res) => {
+    const { name, email, password, location, programs, description, phone } = req.body
+    const UniversityFound = await UnivModel.findOne({ email })
+    if (UniversityFound) {
+        res.status(409).send({ "message": "Already University registered" })
+    }
+    else {
+        try {
+            let dateFormat = moment().format('D-MM-YYYY');
+
+            bcrypt.hash(password, 5, async function (err, hash) {
+                const data = new UnivModel({ name, email, password, location, programs, description, phone, registeredDate: dateFormat })
+                await data.save()
+                res.status(201).send({ "message": "University Registered" })
+            });
+        }
+        catch (err) {
+            res.status(500).send({ "ERROR": err })
+        }
+    }
+}
+
+
+const univLogin = async (req, res) => {
+    const { email, password } = req.body
+    let data = await UnivModel.findOne({ email })
+    if (!data) {
+        return res.send({ "message": "No user found" })
+    }
+    try {
+        bcrypt.compare(password, data.password, function (err, result) {
+            if (result) {
+                var token = jwt.sign({ UniversityID: data._id }, process.env.key);
+
+                res.status(201).send({
+                    "message": "Validation done",
+                    "token": token,
+                    "name": data.name,
+                    "id": data._id
+                })
+            }
+            else {
+                res.status(401).send({ "message": "INVALID credentials" })
+            }
+        });
+    }
+    catch (err) {
+        res.status(500).send({ "ERROR": err })
+    }
+}
+
+
+const univPatch = async (req, res) => {
+    const ID = req.params.id;
+    const payload = req.body;
+    try {
+        await UnivModel.findByIdAndUpdate({ _id: ID }, payload)
+        res.send({ "message": "Database modified" })
+    }
+    catch (err) {
+        console.log(err)
+        res.send({ "message": "error" })
+    }
+}
+
+
+const univDelete = async (req, res) => {
+    const ID = req.params.id;
+    try {
+        await UnivModel.findByIdAndDelete({ _id: ID })
+        res.send({ "message": "Database modified" })
+    }
+    catch (err) {
+        console.log(err)
+        res.send({ "message": "error" })
+    }
+}
+
+
+const univGet = async (req, res) => {
+    try {
+        let data;
+        const id = req.query.id;
+
+        if (id) {
+            data = await UnivModel.find({ _id: id })
+        } else {
+            data = await UnivModel.find()
+        }
+        res.status(200).send({ "Universitys": data })
+    }
+    catch (err) {
+        res.status(500).send({ "ERROR": err })
+    }
+}
+
+
+module.exports = {
+    univRegister, univLogin, univPatch, univDelete, univGet
+}
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,115 +1,27 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const { userRegister, userLogin, userPatch, userDelete, userGet } = require("../controllers/userController")
 const userRoute = express.Router();
 userRoute.use(express.json());
-const moment = require("moment");
-
-require("dotenv").config();
-
-const { StudModel } = require("../model/StudModel");
 
 
 // to register student and then hashing password using Bcrypt
-userRoute.post("/register", async (req, res) => {
-    const { name, email, password, subjects, university } = req.body
-    const studentFound = await StudModel.findOne({ email })
-    if (studentFound) {
-        res.status(409).send({ "message": "Already student registered" })
-    }
-    else {
-        try {
-            let dateFormat = moment().format('D-MM-YYYY');
-
-            bcrypt.hash(password, 5, async function (err, hash) {
-                const data = new StudModel({ name, email, password: hash, subjects, university, registeredDate: dateFormat })
-                await data.save()
-                res.status(201).send({ "message": "Student Registered" })
-            });
-        }
-        catch (err) {
-            res.status(500).send({ "ERROR": err })
-        }
-    }
-})
+userRoute.post("/register", userRegister)
 
 
 // to let student login and then create and send token as response
-userRoute.post("/login", async (req, res) => {
-    const { email, password } = req.body
-    let data = await StudModel.findOne({ email })
-    if (!data) {
-        return res.send({ "message": "No user found" })
-    }
-    try {
-        bcrypt.compare(password, data.password, function (err, result) {
-            if (result) {
-                var token = jwt.sign({ StudentID: data._id }, process.env.key);
-
-                res.status(201).send({
-                    "message": "Validation done",
-                    "token": token,
-                    "name": data.name,
-                    "id": data._id
-                })
-            }
-            else {
-                res.status(401).send({ "message": "INVALID credentials" })
-            }
-        });
-    }
-    catch (err) {
-        res.status(500).send({ "ERROR": err })
-    }
-})
+userRoute.post("/login", userLogin)
 
 
-userRoute.patch("/update/:id", async (req, res) => {
-    const ID = req.params.id;
-    const payload = req.body;
-    try {
-        await StudModel.findByIdAndUpdate({ _id: ID }, payload)
-        res.send({ "message": "Database modified" })
-    }
-    catch (err) {
-        console.log(err)
-        res.send({ "message": "error" })
-    }
-})
+// update particular Student 
+userRoute.patch("/update/:id", userPatch)
 
 
-userRoute.delete("/delete/:id", async (req, res) => {
-    const ID = req.params.id;
-    try {
-        await StudModel.findByIdAndDelete({ _id: ID })
-        res.send({ "message": "Database modified" })
-    }
-    catch (err) {
-        console.log(err)
-        res.send({ "message": "error" })
-    }
-})
+// delete particular Student 
+userRoute.delete("/delete/:id", userDelete)
 
 
 // get particular Student or all students
-userRoute.get("/", async (req, res) => {
-    try {
-        let data;
-        const id = req.query.id;
-
-        if (id) {
-            data = await StudModel.find({ _id: id }).populate("university");
-        } else {
-            data = await StudModel.find().populate("university");
-        }
-        res.status(200).send({ "Students": data })
-    }
-    catch (err) {
-        res.status(500).send({ "ERROR": err })
-    }
-})
-
-
+userRoute.get("/", userGet)
 
 
 module.exports = {
